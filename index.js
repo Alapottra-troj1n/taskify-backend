8681106d885cba907eaa5aed736cb0a81732c73f
Alapottra <alapottratrojon@gmail.com>
2022-05-18 22:06:10 +0600

    added taskask route and jwt sign

--- index.js
+++ index.js
@@ -9,7 +9,41 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://taskify:${process.env.DB_PASS}@cluster0.ifrt0.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const run = async() =>{
+
+    try{
+
+        await client.connect();
+        const database = client.db('taskify');
+        const tasksCollection = database.collection('tasks');
+
+        app.post('/getToken', async (req, res) => {
+            const userEmail = req.body;
+            const jwtToken = jwt.sign(userEmail, process.env.TOKEN_SECRET);
+            res.send({ jwtToken });
+        })
+
+
+        app.post('/addtask', async(req,res)=>{
+
+                const task = req.body;
+                const result = await tasksCollection.insertOne(task);
+                res.send(result)
+        });
+
+
+    }
+    finally{
+
+
+
+    }
+}
+run().catch(console.dir);
 
 
 
